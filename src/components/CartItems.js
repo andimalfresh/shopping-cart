a3be3e46fb81d666c8171691833d141c254d054f
andimalfresh <w.andrew.pedersen@gmail.com>
2018-10-11 11:48:34 -0600

    Initial Commit for shopping-cart

---
+++ src/components/CartItems.js
@@ -0,0 +1,18 @@
+import React from 'react';
+const CartItems = () => {
+    const CartItemComponent = {}
+    return (
+        <div className="container">
+            <h1>Cart Items</h1>
+            <div class="collection-item">
+                <div class="row">
+                    <div class="col-md-8">Mediocre Iron Watch</div>
+                    <div class="col-md-2">$3.99</div>
+                    <div class="col-md-2">1</div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+export default CartItems
