eb2403041ea1092e1200a3ca765d422864fa69d0
andimalfresh <w.andrew.pedersen@gmail.com>
2018-10-15 12:58:09 -0600

    list up

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,13 +1,23 @@
 import React from 'react'
 
-const CartItem = () => {
+const CartItem = (props) => {
+    const cartList = props.cartItemsList.map(itemPrint => {
+        return (
+            <div class="list-group-item">
+                <div className="row">
+                    <div class="col-md-8">{itemPrint.product.name}</div>
+                    <div class="col-md-2">{itemPrint.product.priceInCents}</div>
+                    <div class="col-md-2">{itemPrint.quantity}</div>
+                </div>
+            </div>
+        )
+
+    })
+
     return (
-        <div class="collection-item">
-            <div class="col-md-8">Mediocre Iron Watch</div>
-            <div class="col-md-2">$3.99</div>
-            <div class="col-md-2">1</div>
+        <div className="collection-item">
+            {cartList}
         </div>
     )
 }
-
 export default CartItem
